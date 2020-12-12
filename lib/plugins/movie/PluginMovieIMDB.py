11acedd3b892804e58909940c136dbe8959493fa
Douglas Lindquist <40523253+Dougis1@users.noreply.github.com>
2020-12-11 22:11:07 -0500

    Add files via upload

--- lib/plugins/movie/PluginMovieIMDB.py
+++ lib/plugins/movie/PluginMovieIMDB.py
@@ -1,169 +1,89 @@
 # -*- coding: UTF-8 -*-
 
-__revision__ = '$Id: PluginMovieIMDB.py 1660 2014-03-13 20:48:05Z mikej06 $'
+__revision__ = '$Id: PluginMovieIMDB.py 1660 2020-12-13 dougl $'
+#               Updated to Gtk 3 2020 by Doug Lindquist
 
-# Copyright (c) 2005-2013 Vasco Nunes, Piotr Ożarowski
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+# Copyright 2020 Doug Lindquist doug.lindquist@protonmail.com
 
-# You may use and distribute this software under the terms of the
-# GNU General Public License, version 2 or later
+# Permission is hereby granted, free of charge, to any person obtaining
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import gutils, movie
-import string, re
+import string, re, math
 
 plugin_name         = 'IMDb'
 plugin_description  = 'Internet Movie Database'
 plugin_url          = 'www.imdb.com'
 plugin_language     = _('English')
-plugin_author       = 'Vasco Nunes, Piotr Ożarowski'
-plugin_author_email = 'griffith@griffith.cc'
-plugin_version      = '1.16'
+plugin_author       = 'Doug Lindquist'
+plugin_author_email = 'doug.lindquist@protonmail.com'
+plugin_version      = '1.20'
 
 class Plugin(movie.Movie):
+    from imdb import IMDb
+    ia = None
+    movie = None
+
     def __init__(self, id):
         self.encode   = 'utf-8'
         self.movie_id = id
         self.url      = "http://imdb.com/title/tt%s" % self.movie_id
+        if not ia:
+            ia = IMDb()
 
     def initialize(self):
-        self.cast_page = self.open_page(url=self.url + '/fullcredits')
-        self.plot_page = self.open_page(url=self.url + '/plotsummary')
-        self.comp_page = self.open_page(url=self.url + '/companycredits')
-        self.tagl_page = self.open_page(url=self.url + '/taglines')
-        self.cert_page = self.open_page(url=self.url + '/parentalguide')
-        self.release_page = self.open_page(url=self.url + '/releaseinfo')
+        movie = ia.get_movie(self.movie_id)
+        company = ia.get_company(self.movie_id)
 
     def get_image(self):
-        self.image_url = ''
-        tmp = gutils.trim(self.page, '<div class="poster">', '</div>')
-        if tmp:
-            self.image_url = gutils.trim(tmp, 'src="', '"')
+        try:
+            self.image_url = movie['cover url']
+            if not self.image_url:
+                self.image_url = movie['full-size cover url']
+        except:
+            self.image_url = ''
 
     def get_o_title(self):
-        # it seems, that films coming from the German branch can have their German title in the h1-name-tag;
-        # in this case (only?), IMDB renders an additional "originalTitle"-tag.
-        self.o_title = gutils.trim(self.page, '<div class="originalTitle">', '<span')
-        if not self.o_title:
-            self.o_title = gutils.regextrim(self.page, '<h1 itemprop="name"[^>]*>', '&nbsp;')
-        if not self.o_title:
-            self.o_title = gutils.trim(self.page, 'og:title\' content="', '"')
-        if not self.o_title:
-            self.o_title = re.sub(' [(].*', '', gutils.trim(self.page, '<title>', '</title>'))
-        self.o_title = gutils.clean(re.sub('"', '', self.o_title))
+        self.title = movie['original title']
 
     def get_title(self):
-        # let's try to find the right language version; if not found, the original title succeeds
-        from locale import getdefaultlocale
-        defaultLang, defaultEnc = getdefaultlocale()
-        if defaultLang[:2] == 'cs':
-            self.title = gutils.regextrim(self.release_page,'<td>Czech Republic.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'de':
-            self.title = gutils.regextrim(self.release_page,'<td>Germany.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'es':
-            self.title = gutils.regextrim(self.release_page,'<td>Spain.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'fr':
-            self.title = gutils.regextrim(self.release_page,'<td>France.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'he':
-            self.title = gutils.regextrim(self.release_page,'<td>Israel.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'hu':
-            self.title = gutils.regextrim(self.release_page,'<td>Hungary.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'it':
-            self.title = gutils.regextrim(self.release_page,'<td>Italy.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'pl':
-            self.title = gutils.regextrim(self.release_page,'<td>Poland.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'rm':
-            self.title = gutils.regextrim(self.release_page,'<td>Romania.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'ru':
-            self.title = gutils.regextrim(self.release_page,'<td>Russia.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'sl':
-            self.title = gutils.regextrim(self.release_page,'<td>Slovenia.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'sl':
-            self.title = gutils.regextrim(self.release_page,'<td>Slovakia.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'sr':
-            self.title = gutils.regextrim(self.release_page,'<td>Serbia.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'tk':
-            self.title = gutils.regextrim(self.release_page,'<td>Turkey.*<\/td>\n.*<td>','</td>')
-        elif defaultLang[:2] == 'uk':
-            self.title = gutils.regextrim(self.release_page,'<td>Ukraine.*<\/td>\n.*<td>','</td>')
-
-        if not self.title:
-            self.title = gutils.regextrim(self.page, '<h1>', '([ ]|[&][#][0-9]+[;])<span')
-        if not self.title:
-            self.title = re.sub(' [(].*', '', gutils.trim(self.page, '<title>', '</title>'))
+        self.title = self.movie['title']
 
     def get_director(self):
-        self.director = ''
-        parts = re.split('<a href=', gutils.trim(self.cast_page, 'Directed by', '</table>'))
-        if len(parts) > 1:
-            for part in parts[1:]:
-                director = string.strip(string.replace(gutils.trim(part, '>', '<'), '\n', ''))
-                self.director = self.director + director + ', '
-            self.director = self.director[0:len(self.director) - 2]
+        self.director = gutils.listToString(movie['directors'], joiner=', ')
 
     def get_plot(self):
-        plotlist = string.split(gutils.trim(self.plot_page, 'id="plot-summaries-content">', '</ul>'), '<li')
-        plotcompilation = ''
-        for listelement in plotlist:
-            if listelement <> '' and not 'It looks like we don\'t have any Plot Summaries for this title yet.' in listelement:
-                plotcompilation = plotcompilation + gutils.trim(listelement, '<p>', '</p>') + '\n'
-                plotcompilation = plotcompilation + re.sub('<[^<]+?>', '', gutils.trim(listelement, '<div class="author-container">', '</div>').replace('\n','').lstrip()) + '\n\n'
-        if plotcompilation <> '':
-            self.plot = plotcompilation
-        else:
-            self.plot = gutils.regextrim(self.page, 'itemprop="description"', '<')
-            self.plot = gutils.after(self.plot, '>')
-            elements = string.split(self.plot_page, '<p class="plotpar">')
-            if len(elements) < 2:
-                elements = re.split('<li class="(?:odd|even)">', self.plot_page)
-            if len(elements) > 1:
-                self.plot = self.plot + '\n\n'
-                elements[0] = ''
-                for element in elements[1:]:
-                    if element <> '':
-                        self.plot = self.plot + gutils.strip_tags(gutils.before(element, '</a>')) + '\n\n'
+        self.plot = movie['plot']
+        self.plot.replace('\\', '')
+        self.plot = self.plot.split('::')[0]
 
     def get_year(self):
-        self.year = gutils.trim(self.page, '<a href="/year/', '</a>')
-        self.year = gutils.after(self.year, '>')
-        if not self.year:
-            tmp = gutils.trim(self.page, '<title>', '</title>')
-            tmp = re.search('([0-9]{4})[)]', tmp)
-            if tmp:
-                self.year = tmp.group(0)
+        self.year = movie['year']
 
     def get_runtime(self):
-        self.runtime = gutils.regextrim(self.page, 'Runtime:<[^>]+>', ' min')
+        self.runtime = movie['runtimes']
 
     def get_genre(self):
-        self.genre = string.replace(gutils.regextrim(self.page, 'Genre[s]*:<[^>]+>', '</div>'), '\n', '')
-        self.genre = self.__before_more(self.genre)
+        self.genre = gutils.listToString(movie['genre'], joiner=' | ')
 
     def get_cast(self):
-        self.cast = ''
-        self.cast = gutils.trim(self.cast_page, '<table class="cast_list">', '</table>')
-        if self.cast == '':
-            self.cast = gutils.trim(self.page, '<table class="cast">', '</table>')
-        self.cast = string.replace(self.cast, ' ... ', _(' as '))
-        self.cast = string.replace(self.cast, '...', _(' as '))
-        self.cast = string.replace(self.cast, "\n", '')
-        self.cast = string.replace(self.cast, '</tr>', "\n")
-        self.cast = self.__before_more(self.cast)
-        self.cast = gutils.clean(self.cast)
-        self.cast = re.sub('[ \t]+', ' ', self.cast)
-        self.cast = re.sub(' \n ', '\n', self.cast)
+        self.cast = gutils.listToString(movie['cast'], joiner='\n')
 
     def get_classification(self):
         # until we can find a way to locate the user, we have to use the US-classification
@@ -185,6 +105,7 @@ class Plugin(movie.Movie):
                 entry = string.strip(string.replace(gutils.trim(entry, '>', '<'), '\n', ''))
                 if entry:
                     self.studio = self.studio + entry + ', '
+
             if self.studio:
                 self.studio = self.studio[:-2]
 
@@ -198,68 +119,47 @@ class Plugin(movie.Movie):
         self.trailer = "http://www.imdb.com/title/tt%s/trailers" % self.movie_id
 
     def get_country(self):
-        self.country = '<' + gutils.trim(self.page, 'Country:<', '</div>')
-        self.country = re.sub('[ ]+', ' ', re.sub('[\n]+', '', self.country))
+        self.country = gutils.listToString(movie['countries'], joiner=', ')
 
     def get_rating(self):
-        self.rating = gutils.trim(self.page, 'Users rated this ', '/')
+        self.rating = movie['rating']
         if self.rating:
             try:
-                self.rating = round(float(self.rating), 0)
-            except Exception, e:
+                self.rating = math.trunc(float(self.rating) + 0.5)
+            except Exception as e:
                 self.rating = 0
         else:
             self.rating = 0
 
     def get_notes(self):
         self.notes = ''
-        language = gutils.regextrim(self.page, 'Language:<[^>]+>', '</div>')
-        language = gutils.strip_tags(language)
-        language = re.sub('[\n]+', '', language)
-        language = re.sub('[ ]+', ' ', language)
-        language = language.strip()
-        color = gutils.regextrim(self.page, 'Color:<[^>]+>', '</div>')
-        color = gutils.strip_tags(color)
-        color = re.sub('[\n]+', '', color)
-        color = re.sub('[ ]+', ' ', color)
-        color = color.strip()
-        sound = gutils.regextrim(self.page, 'Sound Mix:<[^>]+>', '</div>')
-        sound = gutils.strip_tags(sound)
-        sound = re.sub('[\n]+', '', sound)
-        sound = re.sub('[ ]+', ' ', sound)
-        sound = sound.strip()
-        tagline = gutils.regextrim(self.tagl_page, '>Taglines', '>See also')
-        taglines = re.split('<div[^>]+class="soda[^>]*>', tagline)
-        tagline = ''
-        if len(taglines)>1:
-            for entry in taglines[1:]:
-                entry = gutils.clean(gutils.before(entry, '</div>'))
-                if entry:
-                    tagline = tagline + entry + '\n'
-        if len(language)>0:
+        language = get_language()
+        color = movie['color']
+        sound = movie['sound mix']
+
+#        tagline = gutils.regextrim(self.tagl_page, '>Taglines', '>See also')
+#        taglines = re.split('<div[^>]+class="soda[^>]*>', tagline)
+#        tagline = ''
+#        if len(taglines)>1:
+#            for entry in taglines[1:]:
+#                entry = gutils.clean(gutils.before(entry, '</div>'))
+#                if entry:
+#                    tagline = tagline + entry + '\n'
+
+        if len(language) > 0:
             self.notes = "%s: %s\n" %(_('Language'), language)
-        if len(sound)>0:
+
+        if len(sound) > 0:
             self.notes += "%s: %s\n" %(gutils.strip_tags(_('<b>Audio</b>')), sound)
-        if len(color)>0:
+
+        if len(color) > 0:
             self.notes += "%s: %s\n" %(_('Color'), color)
-        if len(tagline)>0:
-            self.notes += "%s: %s\n" %('Tagline', tagline)
+
+#        if len(tagline) > 0:
+#            self.notes += "%s: %s\n" %('Tagline', tagline)
 
     def get_screenplay(self):
-        self.screenplay = ''
-        parts = re.split('<a href=', gutils.trim(self.cast_page, '>Writing Credits', '</table>'))
-        if len(parts) > 1:
-            for part in parts[1:]:
-                screenplay = string.strip(string.replace(gutils.trim(part, '>', '<'), '\n', ''))
-                if screenplay == 'WGA':
-                    continue
-                screenplay = screenplay.replace(' (written by)', '')
-                screenplay = screenplay.replace(' and<', '<')
-                if screenplay not in self.screenplay:
-                    self.screenplay = self.screenplay + screenplay + ', '
-            if len(self.screenplay) > 2:
-                self.screenplay = self.screenplay[0:len(self.screenplay) - 2]
-                self.screenplay = re.sub('[ \t]+', ' ', self.screenplay)
+        self.screenplay = movie['writer']
 
     def get_cameraman(self):
         self.cameraman = ''
@@ -283,6 +183,7 @@ class Plugin(movie.Movie):
 class SearchPlugin(movie.SearchMovie):
     PATTERN = re.compile(r"""<a href=['"]/title/tt([0-9]+)/[^>]+[>](.*?)</td>""")
     PATTERN_DIRECT = re.compile(r"""value="/title/tt([0-9]+)""")
+    movies = [""]
 
     def __init__(self):
         # http://www.imdb.com/List?words=
@@ -299,27 +200,29 @@ class SearchPlugin(movie.SearchMovie):
     def search(self,parent_window):
         if not self.open_search(parent_window):
             return None
-        return self.page
+
+        movies = ia.search_movie(self.title)
+        print("2 %d" % len(movies))
+        return len(movies)
 
     def get_searches(self):
-        elements = string.split(self.page, '<tr')
-        if len(elements):
-            for element in elements[1:]:
-                match = self.PATTERN.findall(element)
-                if len(match) > 1:
-                    tmp = re.sub('^[0-9]+[.]', '', gutils.clean(match[1][1]))
-                    self.ids.append(match[1][0])
-                    self.titles.append(tmp)
-        if len(self.ids) < 2:
+        print("1 %s" % self.title)
+        movies = ia.search_movie(self.title)
+        print("5 %d" % len(movies))
+        for m in movies:
+            ids.append(m.movieID)
+            titles.append(m['title'])
+        print("6 %d" % len(ids))
+
+        if len(ids) < 2:
             # try to find a direct result
             match = self.PATTERN_DIRECT.findall(self.page)
             if len(match) > 0:
-                self.ids.append(match[0])
+                ids.append(match[0])
             else:
                 # try to look for IMDb id directly
                 if len(self.title) == 7 and re.match('[0-9]+', self.title):
-                    self.ids.append(self.title)
-
+                    ids.append(self.title)
 
 #
 # Plugin Test
